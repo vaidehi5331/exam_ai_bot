6fca46d9c250bf37d1fc6f38e4df32788ef3e6ff
vaidehi5331 <galphadevaidehi@gmail.com>
2026-07-06 12:11:09 +0530

    Move News API key to environment variable

--- services/news.py
+++ services/news.py
@@ -1,13 +1,16 @@
+import os
 import requests
 
 def get_news(category):
 
-    url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5db3fba8ee0f452a93aa304bc8a9c1d1"
+    api_key = os.getenv("NEWS_API_KEY")
+
+    url = f"https://newsapi.org/v2/top-headlines?country=in&category={category}&apiKey={api_key}"
 
     response = requests.get(url)
 
     data = response.json()
 
-    articles = data["articles"]
+    articles = data.get("articles", [])
 
     return articles
