a090fc7ccbac79417b9ce8a3c4144921e1281a2a
vaidehi5331 <galphadevaidehi@gmail.com>
2026-07-06 14:31:28 +0530

    Use Render PostgreSQL

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 dynamic_answers = []
 current_topic = ""
 quiz_answers = [
@@ -15,7 +16,21 @@ app = Flask(__name__)
 
 app.secret_key = "mysecretkey"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres123@localhost/exambot"
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres123@localhost/exambot"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
